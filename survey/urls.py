bef34b89cfc1488bfa0867b8a3c1512fdb8b6b8a
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-15 22:47:41 +0430

    setting template and views

--- survey/urls.py
+++ survey/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from survey.views import *
+from . import views
+
+app_name = 'survey'
 urlpatterns = [
-    path('',survey_view , name='survy')
+	path('', views.index, name ='index'),
+	path('<int:question_id>/', views.detail, name ='detail'),
+	path('<int:question_id>/results/', views.results, name ='results'),
+	path('<int:question_id>/vote/', views.vote, name ='vote'),
 ]
