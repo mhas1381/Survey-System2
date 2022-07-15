8a6ab0f78ca20fa560ae3837bb6612ab5591ac28
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-07-15 22:14:26 +0430

    customizing admin panel

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
 
+
+from django.contrib import admin
 # Register your models here.
+from .models import Question, Choice
+  
+# admin.site.register(Question)
+# admin.site.register(Choice)
+  
+  
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+  
+  
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),  ]
+    inlines = [ChoiceInLine]
+  
+  
+admin.site.register(Question, QuestionAdmin)
